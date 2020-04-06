9c23ef4e80643c0e2f4afcc983c345d355be1be8
Primarter <alec.ferrari@epitech.eu>
2020-04-06 22:16:54 +0200

    clean(alec): removed unnecessary functions

--- src/clock/clock.c
+++ src/clock/clock.c
@@ -7,30 +7,6 @@
 
 #include "rpg.h"
 
-void clock_display(rpg_t *rpg)
-{
-    float seconds = 0;
-
-    rpg->clock_display.time = sfClock_getElapsedTime(rpg->clock_display.clock);
-    seconds = rpg->clock_display.time.microseconds / 10000;
-    if (seconds > 1) {
-        display(rpg);
-        sfClock_restart(rpg->clock_display.clock);
-    }
-}
-
-void clock_player(rpg_t *rpg)
-{
-    float seconds = 0;
-
-    rpg->clock_player.time = sfClock_getElapsedTime(rpg->clock_player.clock);
-    seconds = rpg->clock_player.time.microseconds / 10000;
-    // if (seconds > 1) {
-    //     move_player(rpg);
-    //     sfClock_restart(rpg->clock_player.clock);
-    // }
-}
-
 void clock_move_rect_player(rpg_t *rpg)
 {
     float seconds = 0;
